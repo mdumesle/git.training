a3c69db458378180fd8fadf65217cbb4f2d13ffa
mdumesle <mdumesle@mdlive>
2016-02-04 01:37:21 -0500

    create api methods

--- mark_2.rb
+++ mark_2.rb
@@ -1,2 +1,33 @@
-name="mark"
+name="mark lee"
 puts name
+puts name.class
+puts name.capitalize
+puts name.reverse
+num= name.count "ys"
+puts num.class
+puts name.bytesize
+puts name.center(10)
+puts name.center(1234)
+puts name.chomp("rk")
+puts name.swapcase
+puts name.partition("r")
+puts name.squeeze
+puts name.succ
+a="Is it Friday yet?"
+b="5:00 o'clock where are you?"
+c="Hello... It's me..."
+puts a
+puts b
+puts c
+
+puts c + name.to_s
+d="I was wondering"
+puts name * 3
+puts name.succ
+puts name.intern
+puts name.sub(/[aeiou]/, '*')
+puts name.sub(/[aeiourk]/, '***')
+puts name.delete "ar"
+puts name.delete "a-kr"
+puts name.delete "ark"
+puts name.count "alee"
